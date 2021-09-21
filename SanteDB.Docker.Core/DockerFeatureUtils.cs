af566151e8578a42f178fe36aeb5ab2b49c23138
Justin Fyfe <justin@fyfesoftware.ca>
2021-09-21 11:34:21 -0400

    Code cleanup

--- SanteDB.Docker.Core/DockerFeatureUtils.cs
+++ SanteDB.Docker.Core/DockerFeatureUtils.cs
@@ -20,8 +20,6 @@
  */
 using SanteDB.Core.Configuration;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace SanteDB.Docker.Core
 {
--- SanteDB.Docker.Core/IDockerFeature.cs
+++ SanteDB.Docker.Core/IDockerFeature.cs
@@ -21,7 +21,6 @@
 using SanteDB.Core.Configuration;
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace SanteDB.Docker.Core
 {
