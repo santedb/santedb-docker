451064aa82af52999bd461eec73ffd5a6ebd3145
Justin Fyfe <justin@fyfesoftware.ca>
2023-05-04 22:07:16 -0400

    Updated to fix the default of CORFE

--- SanteDB.Docker.Core/DockerConfigurationManager.cs
+++ SanteDB.Docker.Core/DockerConfigurationManager.cs
@@ -76,7 +76,7 @@ namespace SanteDB.Docker.Core
                     this.m_configuration = SanteDBConfiguration.Load(s);
                 }
 
-                var enabledFeatures = Environment.GetEnvironmentVariable($"CORE;{DockerConstants.EnvFeatureList}")?.Split(';');
+                var enabledFeatures = $"CORE;{Environment.GetEnvironmentVariable($"{DockerConstants.EnvFeatureList}")}".Split(';');
                 if (enabledFeatures == null || enabledFeatures.Length == 0)
                 {
                     throw new InvalidOperationException($"No features configured - use {DockerConstants.EnvFeatureList}");
